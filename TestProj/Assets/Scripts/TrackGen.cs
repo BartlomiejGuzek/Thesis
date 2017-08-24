05e8334f8ede0969f77ac8dd4fea92a9e8408eb3
Bartłomiej Guzek <bartekguzek@gmail.com>
2017-08-24 14:47:37 +0200

    Started TrackGen

---
+++ TestProj/Assets/Scripts/TrackGen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGen : MonoBehaviour
+{
+    public GameObject trackPrefab;
+    public int trackLenght = 20;
+    public int trackOffset = 16;
+
+    void Start ()
+    {
+        for (int i = 0; i < trackLenght; i++)
+        {
+            float angle = i * Mathf.PI * 2 / trackLenght;
+            //Vector3 position = new Vector3(Game);
+           // Instantiate(trackPrefab, position, Quaternion.identity);
+        }
+
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
