b0e664b84b8de3f61bf53c1542f989dc1299a116
Bartłomiej Guzek <bartekguzek@gmail.com>
2017-08-25 00:13:52 +0200

    Finished first stage of TrackGen
    I can now generate a track with zappers randomly spawning

---
+++ TestProj/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
+        //transform.position = Vector3.Lerp(new Vector3(0,0, -35), new Vector3(0, 10000, -35),  smoothSpeed);
+        //transform.LookAt(target);
+    }
+
+}
--- TestProj/Assets/Scripts/TrackGen.cs
+++ TestProj/Assets/Scripts/TrackGen.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class TrackGen : MonoBehaviour
 {
-    public GameObject trackPrefab;
-    public int trackLenght = 20;
-    public int trackOffset = 16;
+    //Track related variables
+    [SerializeField]
+    private GameObject trackPrefab;
+    [SerializeField]
+    private int trackLenght = 20;
+    private Vector3 trackOffset = new Vector3(0, 16, 0);
+    private Vector3 actualOffset;
+    //Enemies related variables
+    [SerializeField]
+    private GameObject zapperPrefab;
+    [SerializeField]
+    private int zapperAmmount = 20;
+
 
     void Start ()
     {
-        for (int i = 0; i < trackLenght; i++)
+        //Instantiate trackPrefab, trackLenght times
+        for (int i = 0; i < trackLenght; ++i)
         {
-            float angle = i * Mathf.PI * 2 / trackLenght;
-            //Vector3 position = new Vector3(Game);
-           // Instantiate(trackPrefab, position, Quaternion.identity);
+            GameObject trackPart = Instantiate(trackPrefab, GetComponent<Transform>().position + actualOffset, Quaternion.identity) as GameObject;
+            actualOffset = actualOffset + trackOffset;
+            trackPart.transform.Rotate(-90, 0, 180);
+        }
+        for (int i = 0; i < zapperAmmount; ++i)
+        {
+            Vector3 zapperPosition = new Vector3(Random.Range(-6, 4), Random.Range(5, 350), -2.6f);
+            GameObject zapper = Instantiate(zapperPrefab, zapperPosition, Quaternion.identity) as GameObject;
         }
 
     }
